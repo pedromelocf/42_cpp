2baa585390a84b6ebd3ce73ff80c4037db938ef7
Pedro Melo <pedromelocf@yahoo.com>
2024-10-07 13:47:56 -0300

    Inserting ex02

---
+++ cpp1/ex02/src/main.cpp
@@ -0,0 +1,17 @@
+#include "iostream"
+
+int main (void) {
+
+    std::string original = "HI THIS BRAIN";
+    std::string* stringPTR = &original;
+    std::string& stringREF = original;
+
+    std::cout << &original << std::endl;
+    std::cout << &stringPTR << std::endl;
+    std::cout << &stringREF << std::endl;
+
+    std::cout << original << std::endl;
+    std::cout << stringPTR << std::endl;
+    std::cout << stringREF << std::endl;
+
+}
