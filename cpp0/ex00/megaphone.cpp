6cd910eaf7e705b82e0ae6116cdbbf5ebac07f58
Pedro Melo <pedromelocf@yahoo.com>
2024-10-14 10:13:42 -0300

    fixing following eval feedbacks

--- cpp0/ex00/megaphone.cpp
+++ cpp0/ex00/megaphone.cpp
@@ -2,15 +2,18 @@
 
 int main (const int argc, char **argv)
 {
+	std::string argument;
+
 	if (argc == 1)
 		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, 0);
 	for (int i = 1; argv[i] ; i++)
 	{
-		for (int j = 0; argv[i][j]; j++)
-			argv[i][j] = toupper(argv[i][j]);
-		std::cout << argv[i];
-		if (argv[i + 1])
-			std::cout << " ";
+		argument = argv[i];
+		for (long unsigned int j = 0; j < argument.length(); j++)
+		{
+			char c =  std::toupper(argument[j]);
+			std::cout << c;
+		}
 	}
 	std::cout << std::endl;
 	return 0;
