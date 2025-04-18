8d5a0d2b7c0bfe92bee4b6b7d37a24a1ed69e032
Pedro Melo <pedromelocf@yahoo.com>
2025-04-18 20:10:30 -0300

    Refactor iter function and enhance main example: Add null pointer check in iter function and implement Data struct with operator overloads for improved object handling in main.cpp.

--- cpp7/ex01/includes/iter.hpp
+++ cpp7/ex01/includes/iter.hpp
@@ -7,8 +7,10 @@
 
 template <typename T, typename N, typename F>
 void iter (T * arr_addr, N arr_len, F func) {
-    for (N i = 0; i < arr_len ; i++) {
-        func(arr_addr[i]);
+    if (arr_addr != NULL) {
+        for (N i = 0; i < arr_len ; i++) {
+            func(arr_addr[i]);
+        }
     }
 }
 
@@ -22,9 +24,32 @@ template <typename T>
 void uppercase (T & arr_elem) {
 
     static_cast<std::string>(arr_elem);
-    for (size_t i = 0; i < arr_elem.length(); i++){
+    for (size_t i = 0; i < arr_elem.length(); i++) {
         arr_elem[i] = toupper(arr_elem[i]); 
     }
 }
+
+struct Data {
+
+    std::string s;
+    int i;
+
+    Data (const std::string & s, int i) 
+    : s(s), i(i) {};
+
+    Data & operator++ (int) {
+        static Data tmp (*this);
+        this->i++;
+        return tmp;
+    };
+
+
+};
+
+std::ostream & operator<< ( std::ostream& os, const Data & data)
+{
+    os << data.s << " , " << data.i;
+    return os;
+};
 
 #endif 
--- cpp7/ex01/src/main.cpp
+++ cpp7/ex01/src/main.cpp
@@ -16,6 +16,17 @@ int main () {
     ::iter(arr_str, 3, uppercase<std::string>);
     std::cout << std::endl  << "Array string pos: ";
     ::iter(arr_str, 3,  printelement<std::string>);
+    std::cout << std::endl << std::endl;
+
+    std::string * arr_null = NULL;
+    ::iter(arr_null, 3,  printelement<std::string>);
+
+    Data arr_obj[2] = {Data("Hello", 1), Data("World", 2)};
+    std::cout << "Array objects pre: ";
+    ::iter(arr_obj, 2, printelement<Data>);
+    ::iter(arr_obj, 2, increment<Data>);
+    std::cout << std::endl <<"Array objects pos: ";
+    ::iter(arr_obj, 2, printelement<Data>);
     std::cout << std::endl;
 
     return 0;
