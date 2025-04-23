034b6dfb2bcd1f8938163120df4f5d28a5620c99
Pedro Melo <pedromelocf@yahoo.com>
2025-04-22 21:15:38 -0300

    Implement copy constructor and assignment operator for Array class: Enhance memory management with proper deep copy and prevent memory leaks in Array.hpp and main.cpp.

--- cpp7/ex02/Array.hpp
+++ cpp7/ex02/Array.hpp
@@ -15,25 +15,25 @@ class Array {
     public:
         Array() : _array(0), _size(0) {}; 
         Array(unsigned int n) : _array(new T[n]), _size(n) {};
-
         Array(const Array & other) {
-
+            std::cout << "Copy constructor called" << std::endl; 
             _array = new T[other._size];
             _size = other._size;
             for (unsigned int i = 0; i < _size; i++)
                 _array[i] = other._array[i];
         };
 
-        Array & operator=(const Array & other) { 
-            // if (*this->_array)
-            //     delete [] _array;
+        Array & operator=(const Array & other) {
+            std::cout << "Copy assignment operator called" << std::endl;
+            if (this == &other)
+                return *this;
+                
+            this->deletearray();
             _array = new T[other._size];
             _size = other._size;
             for (unsigned int i = 0; i < _size; i++)
                 _array[i] = other._array[i];
-                
             return *this;
-
         };
 
         ~Array() {};
@@ -51,6 +51,13 @@ class Array {
         };
 
         unsigned int getsize() {return this->_size;};
+
+        void deletearray() {
+            if (this->_array != NULL) {
+                delete [] this->_array;
+                this->_array = NULL;
+            }
+        };
 
 };
 
--- cpp7/ex02/main.cpp
+++ cpp7/ex02/main.cpp
@@ -4,8 +4,9 @@ int main()
 {
     std::cout << "   TEST 1: STRING ARRAY " << std:: endl;
     Array<std::string> arr_str(2);
-    arr_str[0] = "Hello";
-    arr_str[1] = "World";
+    arr_str[0] = "1";
+    arr_str[1] = "2";
+
     std::cout << "arr_str[0] = '" << arr_str[0] << "'; arr_str[1] = '" << arr_str[1] << "'" << std::endl;
 
     std::cout << std::endl << "   TEST 2: OUT OF RANGE " << std:: endl;
@@ -31,27 +32,35 @@ int main()
     Array<std::string> arr_str_cpy(arr_str);
 
     std::cout << "Pre change orig:   " << "    arr_str[0] = '" << arr_str[0] << "';        arr_str[1] = '" << arr_str[1] << "'" << std::endl;
-    std::cout << "Pre change cpy:    " << "arr_str_cpy[0] = '" << arr_str_cpy[0] << "';    arr_str_cpy[1] = '" << arr_str_cpy[1] << "'" << std::endl;
+    std::cout << "Pre change cpy:    " << "arr_str_cpy[0] = '" << arr_str_cpy[0] << "';    arr_str_cpy[1] = '" << arr_str_cpy[1] << "'" << std::endl << std::endl;
     
-    arr_str[0] = "Other";
-    arr_str[1] = "Copy";
+    arr_str[0] = "3";
+    arr_str[1] = "4";
 
     std::cout << "Post change orig:  " << "    arr_str[0] = '" << arr_str[0] << "';        arr_str[1] = '" << arr_str[1] << "'" << std::endl;
     std::cout << "Post change cpy:   " << "arr_str_cpy[0] = '" << arr_str_cpy[0] << "';    arr_str_cpy[1] = '" << arr_str_cpy[1] << "'" << std::endl;
 
     std::cout << std::endl << "   TEST 6: ASSIGNMENT OPERATOR " << std:: endl;
-    Array<std::string> arr_str_assign(0);
+    Array<std::string> arr_str_assign(5);
     arr_str_assign = arr_str;
 
     std::cout << "Pre change orig:   " << "       arr_str[0] = '" << arr_str[0] << "';        arr_str[1] = '" << arr_str[1] << "'" << std::endl;
-    std::cout << "Pre change cpy:    " << "arr_str_assign[0] = '" << arr_str_assign[0] << "'; arr_str_assign[1] = '" << arr_str_assign[1] << "'" << std::endl;
+    std::cout << "Pre change cpy:    " << "arr_str_assign[0] = '" << arr_str_assign[0] << "'; arr_str_assign[1] = '" << arr_str_assign[1] << "'" << std::endl << std::endl;
     
-    arr_str[0] = "Copy";
-    arr_str[1] = "Test";
+    arr_str[0] = "5";
+    arr_str[1] = "6";
 
-    std::cout << "Post change orig:  " << "       arr_str[0] = '" << arr_str[0] << "';         arr_str[1] = '" << arr_str[1] << "'" << std::endl;
+    std::cout << "Post change orig:  " << "       arr_str[0] = '" << arr_str[0] << "';        arr_str[1] = '" << arr_str[1] << "'" << std::endl;
     std::cout << "Post change cpy:   " << "arr_str_assign[0] = '" << arr_str_assign[0] << "'; arr_str_assign[1] = '" << arr_str_assign[1] << "'" << std::endl;
 
-    
+    // std::cout << std::endl << "   TEST 7: CONST OVERLOAD " << std:: endl;
+    // const Array<int> const_arr(36) ;
+    // std::cout << const_arr[0] << std::endl;
+
+    arr_str.deletearray();
+    arr_str_assign.deletearray();
+    arr_str_cpy.deletearray();
+    // const_arr.deletearray();
+
     return 0;
 }
