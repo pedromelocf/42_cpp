0340c2110718251fe0b5957f408b9640b40a4075
Pedro Melo <pedromelocf@yahoo.com>
2025-05-23 11:28:48 -0300

    Refactor PmergeMe class: update fordJohnsonSortVector method to use long unsigned int for pair_size and improve variable types for clarity

--- cpp9/ex02/includes/PmergeMe.hpp
+++ cpp9/ex02/includes/PmergeMe.hpp
@@ -4,23 +4,24 @@
 #include <iostream>
 #include <vector>
 #include <deque>
+#include <algorithm>
 
 class PmergeMe {
 
 	private:
-
 		std::vector<int> _vector;
 		std::deque<int> _deque;
 
 	public:
 		PmergeMe();
+		PmergeMe(std::vector<int> to_sort);
 		PmergeMe(const PmergeMe& other);
 		PmergeMe& operator=(const PmergeMe& other);
 		~PmergeMe();
 
-		void	fordJohnsonSortVector(int pair_size);
-		void	fordJohnsonSortDeque(int pair_size);
-		static bool checkSequence();
+		void	fordJohnsonSortVector(long unsigned int pair_size);
+		// void	fordJohnsonSortDeque(int pair_size);
+		// static bool checkSequence();
 };
 
 std::vector<int> generateJacobsthalIndices(size_t limit);
--- cpp9/ex02/src/PmergeMe.cpp
+++ cpp9/ex02/src/PmergeMe.cpp
@@ -1,6 +1,10 @@
-#include "includes/PmergeMe.hpp"
+#include "../includes/PmergeMe.hpp"
 
 PmergeMe::PmergeMe() {}
+
+PmergeMe::PmergeMe(std::vector<int> to_sort) {
+	_vector = to_sort;
+}
 
 PmergeMe::PmergeMe(const PmergeMe& other) {
 	_vector = other._vector;
@@ -17,79 +21,74 @@ PmergeMe& PmergeMe::operator=(const PmergeMe& other) {
 
 PmergeMe::~PmergeMe() {}
 
-void PmergeMe::fordJohnsonSortVector(int pair_size) {
+void PmergeMe::fordJohnsonSortVector(long unsigned int pair_size) {
 
-	while (( _vector.size() / 2) > pair_size ) {
+	while ((_vector.size() / 2) > pair_size) {
 
+        size_t chunks = _vector.size() / pair_size;
+        for (size_t i = 0; chunks > 0; i += pair_size, --chunks) {
+
+            size_t mid = i + pair_size / 2;
+            size_t end = i + pair_size;
+            if (_vector[mid - 1] > _vector[end - 1]) 
+                std::swap_ranges(_vector.begin() + i, _vector.begin() + mid, _vector.begin() + mid );
+        }
+		pair_size *= 2;
+	}
+
+	pair_size /= 2;
+
+	while (pair_size != 2) {
 		int chunks = _vector.size() / pair_size;
-		for (int i = 0; chunks > 0 ; i += pair_size, --chunks) {
+		for (size_t i = 0; chunks > 0; i += pair_size, --chunks) {
 
-			int k = i + pair_size / 2 - 1;
-			int l = i + pair_size - 1;
-			if (_vector.at(k) > _vector.at(l)) {
+			std::vector<int> temp, main, pend, jacobsthal_numbers;
+			int vector_size = _vector.size();
+			for (int m = 0 ; vector_size > m; ++m ) {
 
-				std::vector<int> temp;
-				if (chunks == 1 && _vector.size() % pair_size != 0 ) {
+				if (chunks == 1 && _vector.size() % pair_size != 0 )
+					temp.insert(temp.end(), _vector.begin() + m, _vector.end());
+				else if (m == 0 || m % 2 != 1)
+					main.insert(main.end(), _vector.begin() + m, _vector.begin() + m + pair_size);
+				else
+					pend.insert(pend.end(), _vector.begin() + m, _vector.begin() + m + pair_size );
+			}
+			_vector.clear();
+			jacobsthal_numbers = generateJacobsthalIndices(pend.size());
+			int prev_jacobsthal = jacobsthal_numbers[0];
+			for (int p = 1 ; p < jacobsthal_numbers.size(); p++) {
 
-					temp.insert(temp.end(), _vector.begin() + k + 1 , _vector.end());
-					_vector.erase(_vector.begin() + k + 1, _vector.end());
+				int curr_jacobsthal = jacobsthal_numbers[p];
+				int jacobsthal_dif = curr_jacobsthal - prev_jacobsthal;
+				if (jacobsthal_dif > static_cast<int>(pend.size()))
+					break;
+				int insertion_times = jacobsthal_dif;
+				while(insertion_times > 0) {
+					insertion_times--;
 				}
-				std::swap_ranges(_vector.begin(), _vector.begin() + k, _vector.begin() + l);
-				_vector.insert(_vector.end() - 1, temp.begin(), temp.end());
-				temp.clear();
+				prev_jacobsthal = curr_jacobsthal;
 			}
-		}
-		fordJohnsonSortVector(pair_size * 2);
-	}
+			while (pend.size() > 0 ) {
 
-	int chunks = _vector.size() / pair_size;
-	for (int i = 0; chunks > 0 ; i += pair_size, --chunks) {
-
-		std::vector<int> temp, main, pend, jacobsthal_numbers;
-		int vector_size = _vector.size();
-		for (int m = 0 ; vector_size > m; ++m ) {
-
-			if (chunks == 1 && _vector.size() % pair_size != 0 )
-				temp.insert(temp.end(), _vector.begin() + m, _vector.end());
-			else if (m == 0 || m % 2 != 1)
-				main.insert(main.end(), _vector.begin() + m, _vector.begin() + m + pair_size);
-			else
-				pend.insert(pend.end(), _vector.begin() + m, _vector.begin() + m + pair_size );
-		}
-		_vector.clear();
-		jacobsthal_numbers = generateJacobsthalIndices(pend.size());
-		int prev_jacobsthal = jacobsthal_numbers[0];
-		for (int p = 1;; p++) {
-
-			int curr_jacobsthal = jacobsthal_numbers[p];
-			int jacobsthal_dif = curr_jacobsthal - prev_jacobsthal;
-			if (jacobsthal_diff > static_cast<int>(pend.size()))
-				break;
-			int insertion_times = jacobsthal_diff;
-			while(insertion_times > 0) {
-				insertion_times--;
+				size_t value_to_insert = pend.back();
+				pend.pop_back();
+				main.insert(std::lower_bound(main.begin(), main.end(), value_to_insert), value_to_insert);
 			}
-			prev_jacobsthal = curr_jacobsthal;
+			main.insert(main.end(), temp.begin(), temp.end());
+			_vector = main;
+			main.clear(); temp.clear(); pend.clear();
 		}
-		while (pend.size() > 0 ) {
-			//insert from end to begin of pend to main.
-		}
-		//copy from temp to main;
-		//copy from main to _vector;
-		// main.clear(); temp.clear();
 	}
+	pair_size /= 2;
 }
 
-void PmergeMe::fordJohnsonSortDeque(int pair_size) {
-}
+// void PmergeMe::fordJohnsonSortDeque(int pair_size) {}
 
-bool PmergeMe::checkSequence() {
-
-}
+// bool PmergeMe::checkSequence() {}
 
 std::vector<int> generateJacobsthalIndices(size_t size) {
 
-	std::vector<int> jacobsthal = {1};
+	std::vector<int> jacobsthal(1,1);
 	int j1 = 1, j2 = 0;
 	while (jacobsthal.back() < static_cast<int>(size)) {
 
